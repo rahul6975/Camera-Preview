5add4fe3432ec300822e9205410b5450552140ea
rahul6975 <rahulya7569@gmail.com>
2021-07-31 10:55:37 +0530

    camera permissions completed

--- app/src/main/java/com/rahul/camerasnapshots/MainActivity.kt
+++ app/src/main/java/com/rahul/camerasnapshots/MainActivity.kt
@@ -1,11 +1,120 @@
 package com.rahul.camerasnapshots
 
+import android.Manifest
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    var camera: Camera? = null
+    var preview: Preview? = null
+    var imageCapture: ImageCapture? = null
+
+    val REQESUT_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermissions()
+
+        initViewsAndListeners()
+    }
+
+    private fun initViewsAndListeners() {
+        btnClick.setOnClickListener {
+            takePhotos()
+        }
+    }
+
+    private fun takePhotos() {
+        //save photos
+
+        val photoFile = File(
+            externalMediaDirs.firstOrNull(),
+            "MySavedImages - ${System.currentTimeMillis()}.jpg"
+        )
+
+        val output = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture?.takePicture(
+            output,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(applicationContext, "Image saved", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        applicationContext,
+                        "An error occurred while saving",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+            })
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PERMISSION_GRANTED
+        ) {
+            startCamera()
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQESUT_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PERMISSION_GRANTED
+        ) {
+
+            startCamera()
+
+        } else {
+            Toast.makeText(
+                this,
+                "Permissions are denied, please allow camera permission from settings",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun startCamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            preview = Preview.Builder().build()
+            preview?.setSurfaceProvider(cameraView.createSurfaceProvider(camera?.cameraInfo))
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            cameraProvider.unbindAll()
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+        }, ContextCompat.getMainExecutor(this))
     }
 }
